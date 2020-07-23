99d5989289d8c29dcf6d5885485a384f7a253ff8
Didier Bröska <didier.broska@gmail.com>
2020-07-23 12:15:51 +0200

    Add VScode posh profile

---
+++ Microsoft.VSCode_profile.ps1
@@ -0,0 +1,12 @@
+<#
+ #  VSCode PowerShell Profile
+ #
+ #  Author  :   Didier Bröska - didier.broska@gmail.com
+ #  Date    :   22/07/2020
+ #
+ #>
+
+Import-Module posh-git
+Import-Module oh-my-posh
+
+. "${PSScriptRoot}\Microsoft.PowerShell_profile.ps1"
